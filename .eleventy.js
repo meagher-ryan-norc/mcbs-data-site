68650d13027191e1033246ba94495107fdd772fd
Chad Yoder <cwyoder13@gmail.com>
2021-10-14 12:44:44 -0500

    Initial setup: Added eleventy and sass

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addPassthroughCopy("./src/css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,6 @@
+---
+title: "Hello World"
+layout: "base.njk"
+---
+
+This is a home page. Whee!
