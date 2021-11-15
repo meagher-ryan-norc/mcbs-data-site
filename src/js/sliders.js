3182f904f6d1354fb5930695a6fcd44f1baaba46
Chad Yoder <cwyoder13@gmail.com>
2021-11-15 11:54:12 -0600

    refactor slider hover from JS to CSS

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,20 +1,4 @@
 // CAROUSEL SLIDERS (splide)
-function setPanels () {
-  const infoPanels = document.querySelectorAll('.slide__info');
-
-  for (let i = 0; i < infoPanels.length; i++){
-    setPanelHeight(infoPanels[i]);
-  }
-}
-
-// set panel bottom height
-function setPanelHeight(element) {
-  const panelHeight = element.clientHeight;
-  const panelLabel = element.firstElementChild;
-  const labelHeight = panelLabel.clientHeight;
-  const bottomMeasure = (panelHeight - labelHeight - 23) * -1;
-  element.style.bottom = `${bottomMeasure}px`;
-}
 
 // add sliders to page
 function initSplide() {
@@ -30,40 +14,11 @@ function initSplide() {
       gap: '2rem',
     }).mount();
   }
-  setPanels();
-}
-
-//add mouseover events to panels
-function initPanels() {
-  const panelLinks = document.querySelectorAll('.slide__link');
-  for (let i = 0; i < panelLinks.length; i++){
-    const panel = panelLinks[i].firstElementChild;
-    panelLinks[i].onmouseover = function(){
-      panel.style.bottom = '0px';
-    };
-    panelLinks[i].onmouseout = function(){
-      setPanelHeight(panel);
-    };
-    panelLinks[i].onfocus = function(){
-      panel.style.bottom = '0px';
-    }
-    panelLinks[i].onblur = function(){
-      setPanelHeight(panel);
-    };
-  }
 }
 
 // DOCUMENT LOAD INIT
 document.addEventListener('DOMContentLoaded', function(){
   if (document.querySelector('.splide')) {
     initSplide();
-    initPanels();
   }
-})
-
-// RESIZE WINDOW EVENT
-window.addEventListener('resize', function(){
-  if (document.querySelector('.splide')) {
-    setPanels();
-  }
-})
+});
